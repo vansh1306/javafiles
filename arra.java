c35f4859f1c0e837a5fd2645194d5d59cd5f6dc2
Vansh Srivastava <167678882+vansh1306@users.noreply.github.com>
2025-02-11 22:14:59 +0530

    arra added

---
+++ arra.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.util.Arrays;
+// public class arra
+// {
+//     public static void main(String[] args) {
+//         int n;
+//         System.out.print("Enter size of array:");
+//         Scanner sc=new Scanner(System.in);
+//         n=sc.nextInt();
+//         int[] arr=new int[n];
+//         System.out.println("Enter elements inside the array:- ");
+//         for(int i=0;i<arr.length;i++)
+//         {
+//             Scanner ab=new Scanner(System.in);
+//             arr[i]=sc.nextInt();
+//         }
+//        // System.out.println(Arrays.toString(arr));
+//        for(int j=0;j<arr.length;j++)
+//        {
+//         System.out.print(" "+arr[j]);
+//        }
+//     }
+// }
+
+// 2d array
+
+// public class arra
+// {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+        //int[][] arr=new int[3][];  //this can be a way
+        // int[][] arr3= {{1,2,3},{4,5,6},{7,8,9}};
+        // System.out.println(arr3[0][0]);
+//         int[][] arr=new int[3][];
+//         for(int i=0;i<3;i++)
+//         {
+//             for(int j=0;j<arr[i].length;i++)
+//             {
+//                 arr[i][j]=sc.nextInt();
+//             }
+//         }
+
+//     }
+// }
+
+//array
+
+// public class arra
+// {
+//     public static void main(String[] args)
+//      {
+//         int[] arr=new int[5];
+//         Scanner sc=new Scanner(System.in);
+//         for(int i=0;i<5;i++)
+//         {
+//             arr[i]=sc.nextInt();
+//         }
+//         for(int i=0;i<5;i++)
+//         {
+//             System.out.print(" "+arr[i]);
+//         }
+        
+//     }
+// }
+
+//reverse array
+// public class arra
+// {
+//     public static void main(String[] args) {
+//         int[] arr={4,5,6,7};
+//         int[] temp=new int[4];
+//         for(int i=0;i<=3;i++)
+//         {
+//             temp[i]=arr[4-i-1];
+            
+//         }
+//         System.out.println(Arrays.toString(temp));
+//     }
+// }
+
+// public class arra
+// {
+//         public static void main(String[] args) {
+//                 int n;
+//                 System.out.println("Enter size of array");
+//                 Scanner sc=new Scanner(System.in);
+//                 n=sc.nextInt();
+//                 int[] arr=new int[n];
+//                 for(int i=0;i<n;i++)
+//                 {
+//                         Scanner s=new Scanner(System.in);
+//                         arr[i]=s.nextInt();
+//                 }
+//                 //for(int j=0;j<n;j++)
+//                 //{
+//                         System.out.println(Arrays.toString(arr));
+//                 //}
+//         }
+// }
