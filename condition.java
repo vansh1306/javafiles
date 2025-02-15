b33f41dc0d1f2e8e2d88ba306c67bf90c02cb90c
Vansh Srivastava <167678882+vansh1306@users.noreply.github.com>
2025-02-15 21:00:49 +0530

    conditional operators

---
+++ condition.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class condition {
+public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+//    int a;
+//    System.out.print("Enter n: ");
+//    a=sc.nextInt();
+//    for(int i=1;i<=a;i++)
+//    {
+//     System.out.print(" "+i);
+//    }
+
+        int n;
+        n=sc.nextInt();
+        
+
+}
+
+
+}
