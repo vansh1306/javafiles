a8345cc2abdb09c64e164bc566a800421dbadd73
Vansh Srivastava <167678882+vansh1306@users.noreply.github.com>
2025-02-15 21:01:18 +0530

    num

---
+++ num.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class num {
+    public static void main(String[] args) {
+        int a;
+        Scanner sc= new Scanner(System.in);
+        a=sc.nextInt();
+        for (int i=1;i<=a;i++ ){
+            System.out.println("Number :" + i);
+        }
+    }
+}
