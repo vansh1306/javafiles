ea5fba0ded5de801fc3818e167965f766cbf519e
Vansh Srivastava <167678882+vansh1306@users.noreply.github.com>
2025-03-13 21:24:02 +0530

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -18,7 +18,7 @@ public class Calculate {
     public int findMax() {
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
+            if (arr[i]  > max) {
                 max = arr[i];
             }
         }
